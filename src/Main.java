d2eba5d2eb145169da829228a0ee0a899a86bc58
iMartzen <git@martzen.nl>
2019-09-15 16:19:01 +0200

    Update for main making responsible for input

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,43 @@
 import board.*;
 import players.*;
+import java.util.Scanner;
 import static java.lang.System.exit;
 
 public class Main {
 
-    private static final String PRINT_STARTGAME = "_ _ _ ____ _    ____ ____ _  _ ____    ___ ____    ___ _ ____ ___ ____ ____ ___ ____ ____ \\n" +
-            "| | | |___ |    |    |  | |\\\\/| |___     |  |  |     |  | |     |  |__| |     |  |  | |___ \\n" +
-            "|_|_| |___ |___ |___ |__| |  | |___     |  |__|     |  | |___  |  |  | |___  |  |__| |___ \\n" +
+    private static final String PRINT_HEADER = "_ _ _ ____ _    ____ ____ _  _ ____    ___ ____    ___ _ ____ ___ ____ ____ ___ ____ ____ \n" +
+            "| | | |___ |    |    |  | |\\/| |___     |  |  |     |  | |     |  |__| |     |  |  | |___ \n" +
+            "|_|_| |___ |___ |___ |__| |  | |___     |  |__|     |  | |___  |  |  | |___  |  |__| |___ \n" +
             "                                                                                          ";
+
+    public static final Scanner scanner = new Scanner(System.in);  
+    
     public static void main(String[] args) {
         
         //Start of the game
-        System.out.println(PRINT_STARTGAME);
+        System.out.println(PRINT_HEADER);
         
-        // Create object players 
-        Player player = new Player();
-        
-        //Ask for names
-        Player[] players = player.callPlayers();
-        
+        // Create object player1         
+        System.out.println("Please enter username of player 1:");   
+        String usernamePlayer1 = scanner.nextLine();
+        System.out.println("What sign would you like to play with?");
+        String signPlayer1 = scanner.nextLine();
+        Player player1 = new Player(usernamePlayer1, signPlayer1);
+        System.out.println("Username of player 1 is: " + player1.getUserName() + " and will play with this sign: " + player1.getSign()); 
+
+        //Create object player2
+        System.out.println("Please enter username of player 2:");
+        String usernamePlayer2 = scanner.nextLine();
+        System.out.println("What sign would you like to play with?");
+        String signPlayer2 = scanner.nextLine();
+        Player player2 = new Player(usernamePlayer2, signPlayer2);
+        System.out.println("Username of player 2 is: " + player2.getUserName()+ " and will play with this sign: " + player2.getSign());
+
         //Choose random a beginner
+        Player player = new Player();
+        Player[] players = {player1, player2};
         int playerBeginner = player.randomBeginner(players);
+        System.out.println("--------------------------------");
         System.out.println("The computer randomly chooses who starts first. This player starts: " + players[playerBeginner].getUserName());
         
         //Creates object board
@@ -28,13 +45,11 @@ public class Main {
 
         //Draw the board 
         System.out.println(board.drawBoard());
-        
         //First player starts
         System.out.println(players[playerBeginner].getUserName() + " let's give it a go! On which number would you like your sign?");
-        
+
         //Create winner variable for check if there is already a winner
         String winner = "";
-        
         //Start counter for calculate a draw
         int counter = 0;
 
--- src/players/Player.java
+++ src/players/Player.java
@@ -1,45 +1,33 @@
 package players;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Player {
-
-    Scanner scanner = new Scanner(System.in); 
 
     private String userName;
     private String sign;
 
-    public String getUserName() {
-        return userName;
+    public Player(){}
+    
+    public Player(String userName, String sign){
+        this.userName = userName;
+        this.sign = sign;
     }
 
-    public String getSign() {
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getSign(){
         return sign;
     }
 
-    //This method will call the players and ask for their names 
-    public Player[] callPlayers() {
-
-        //Creating player objects and set the sign per player
-        Player player1 = new Player();
-        player1.sign = "O";
-        Player player2 = new Player();
-        player2.sign = "X";
-
-        //Ask for player name and reads input and will put this is the objects variable username
-        System.out.println("Please enter username of player 1:");                   
-        player1.userName = scanner.nextLine();                                      
-        System.out.println("Username of player 1 is: " + player1.userName);
-
-        //Ask for player name and reads input and will put this is the objects variable username
-        System.out.println("Please enter username of player 2:");                   
-        player2.userName = scanner.nextLine();                                     
-        System.out.println("Username of player 2 is: " + player2.userName);
-
-        //Initialises an array of the 2 player objects and return this list
-        Player[] listPlayers = {player1, player2};
-        return listPlayers;
+    public void setSign(String sign){
+        this.sign = sign;
     }
 
     //This method will picks random the player who may start 
